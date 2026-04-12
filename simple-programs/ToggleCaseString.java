e617c12a09c0a9a89e2015a1da22c5808951a0de
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-12 09:26:39 +0530

    Add a program of toggle case string

---
+++ simple-programs/ToggleCaseString.java
@@ -0,0 +1,19 @@
+public class ToggleCaseString {
+    public static void main(String[] args) {
+        String str="HaMSiNi";
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>='A' && ch<='Z'){
+                sb.append((char)(ch+32));
+            }
+            else if(ch>='a' && ch<='z'){
+                sb.append((char)(ch-32));
+            }else{
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
