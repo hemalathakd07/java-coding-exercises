c08d0f62c8d7d3dc4bfcf634e65fa7bc1f899d54
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-13 15:16:52 +0530

    Add remove all white spaces in a string program

---
+++ simple-programs/RemoveAllWhiteSpacesInAString.java
@@ -0,0 +1,14 @@
+public class RemoveAllWhiteSpacesInAString {
+    public static void main(String[] args) {
+        String str="Hello\t..\nevery one";
+        String replacedString="";
+        int i=0;
+        while(i<str.length()){
+            if(str.charAt(i)!='\t' && str.charAt(i)!=' ' && str.charAt(i)!='\n'){
+                replacedString+=str.charAt(i);
+            }
+            i++;
+        }
+        System.out.println(replacedString);
+    }
+}
