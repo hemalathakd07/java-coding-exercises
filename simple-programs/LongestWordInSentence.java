b064381ab5732b0f38efd1e0930a6e2bb6eaaa58
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-16 16:23:59 +0530

    Add a program to find the longest word in the given string

---
+++ simple-programs/LongestWordInSentence.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LongestWordInSentence {
+    public static void main(String[] args) {
+        String str="Welcome to my Homeeeeeeee!";
+        String longestWord="";
+        String word="";
+        str=str+" ";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch!=' '){
+                word+=ch;
+            }
+            else{
+                if(word.length()>longestWord.length()){
+                    longestWord=word;
+                }
+                word="";
+            }
+        }
+            System.out.println("The longest word in the given string is "+longestWord);
+    }
+}
